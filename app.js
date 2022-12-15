986e973cad8c86b53e203bb965551099db3ef425
BoRuei1028 <andy6o43222@gmail.com>
2022-12-15 11:57:05 +0800

    feat: add Handlebars

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const exphbs = require('express-handlebars')
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
 const app = express()
 const port = 3000
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
@@ -21,8 +20,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('running')
+  res.render('index')
 })
 
 app.listen(port, () => {
