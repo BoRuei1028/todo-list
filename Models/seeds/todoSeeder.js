c27f8cdff8a01a12accec966cb882abb868f4065
BoRuei1028 <andy6o43222@gmail.com>
2022-12-21 14:37:52 +0800

    refactor: modify mongoose config

--- Models/seeds/todoSeeder.js
+++ Models/seeds/todoSeeder.js
@@ -1,18 +1,9 @@
-const mongoose = require('mongoose')
+
 const Todo = require('../todo') //載入 todo model
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
-  // 新增資料腳本
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
