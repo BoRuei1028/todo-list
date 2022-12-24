5f86d4d16ae0b6bd0bbb367520195cb8843c37d8
BoRuei1028 <andy6o43222@gmail.com>
2022-12-24 09:24:21 +0800

    fix: modify home / todos routes modules

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Todo = require('../../Models/todo')
+const Todo = require('../../models/todo')
 router.get('/', (req, res) => {
   Todo.find()
     .lean()
